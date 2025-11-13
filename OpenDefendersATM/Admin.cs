a01fda7a1449378c64a09e730a8c9a27683b2a67
Pontus <Pontus_thoren@hotmail.com>
2025-11-13 11:42:26 +0100

    Added User+Customer+Admin classes

---
+++ OpenDefendersATM/Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDefendersATM
+{
+    internal class Admin:User
+    {
+        public void CreateNewUser()
+        {
+            //Skapar en ny kontohavare
+        }
+        public void CreateNewAdmin()
+        {
+            //Skapar en ny Admin
+        }
+        public void ExChangeRate()
+        {
+            //Växlningskursen
+        }
+    }
+}
