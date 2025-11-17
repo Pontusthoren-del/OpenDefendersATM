b2ec107cc5fe216b8712a7d128c7927cc166b1cf
Pontus <Pontus_thoren@hotmail.com>
2025-11-17 09:36:49 +0100

    Added a template

---
+++ OpenDefendersATM/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDefendersATM
+{
+    internal class Transaction
+    {
+        public int transactionID { get; set; }
+        public List<Account> fromAccount { get; set; }
+        public List<Account> toAccount { get; set; }
+
+        public float amount { get; set; }
+        public string currency { get; set; }
+        public DateTime timestamp { get; set; }
+        public string status { get; set; }
+
+        public void Execute()
+        {
+
+        }
+        public void ScheduleExecute()
+        {
+
+        }
+    }
+}
