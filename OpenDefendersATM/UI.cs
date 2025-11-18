aecde57360d39427bd35d37d8b4ce504f3fced4b
Pontus <Pontus_thoren@hotmail.com>
2025-11-18 14:24:33 +0100

    Small changes

--- OpenDefendersATM/UI.cs
+++ OpenDefendersATM/UI.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace OpenDefendersATM
 {
     internal class UI
     {
-        // User Interface
+        public void RunBankApp()
+        {
+            bool loggedIn = true;
+            int loggedInTries = 0;
+            while (loggedIn && loggedInTries < 3)
+            {
+                if (User.Login())
+                {
+
+                }
+            }
+        }
+        public void ShowMainMenu()
+        {
+
+        }
     }
 }
