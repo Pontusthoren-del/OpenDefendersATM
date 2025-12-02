bcb3e33280aab9b0a69f48ef15da50207e8297d8
Pontus <Pontus_thoren@hotmail.com>
2025-12-02 14:06:46 +0100

    Fixed ViewAccounts and gave all our customers some start accounts.

--- OpenDefendersATM/UI.cs
+++ OpenDefendersATM/UI.cs
@@ -12,7 +12,6 @@ namespace OpenDefendersATM
     {
         public static void RunBankApp()
         {
-            BankSystem.InitializeStartAccounts();
             bool loggedIn = true;
             int loggedInTries = 0;
             while (loggedIn && loggedInTries < 3)
@@ -32,7 +31,6 @@ namespace OpenDefendersATM
                     {
                         Console.WriteLine(new string('-', 30));
                         Console.WriteLine("För många försök. Ditt konto har låsts.");
-                        //Customer.LockedOut();
                         loggedIn = false;
                     }
 
@@ -41,7 +39,7 @@ namespace OpenDefendersATM
 
         }
         //Console.WriteLine("···················································································\r\n: _______  _______  _______  _                                                    :\r\n:(  ___  )(  ____ )(  ____ \\( (    /|                                             :\r\n:| (   ) || (    )|| (    \\/|  \\  ( |                                             :\r\n:| |   | || (____)|| (__    |   \\ | |                                             :\r\n:| |   | ||  _____)|  __)   | (\\ \\) |                                             :\r\n:| |   | || (      | (      | | \\   |                                             :\r\n:| (___) || )      | (____/\\| )  \\  |                                             :\r\n:(_______)|/       (_______/|/    )_)                                             :\r\n:                                                                                 :\r\n: ______   _______  _______  _______  _        ______   _______  _______  _______ :\r\n:(  __  \\ (  ____ \\(  ____ \\(  ____ \\( (    /|(  __  \\ (  ____ \\(  ____ )(  ____ \\:\r\n:| (  \\  )| (    \\/| (    \\/| (    \\/|  \\  ( || (  \\  )| (    \\/| (    )|| (    \\/:\r\n:| |   ) || (__    | (__    | (__    |   \\ | || |   ) || (__    | (____)|| (_____ :\r\n:| |   | ||  __)   |  __)   |  __)   | (\\ \\) || |   | ||  __)   |     __)(_____  ):\r\n:| |   ) || (      | (      | (      | | \\   || |   ) || (      | (\\ (         ) |:\r\n:| (__/  )| (____/\\| )      | (____/\\| )  \\  || (__/  )| (____/\\| ) \\ \\__/\\____) |:\r\n:(______/ (_______/|/       (_______/|/    )_)(______/ (_______/|/   \\__/\\_______):\r\n:                                                                                 :\r\n: _______ _________ _______                                                       :\r\n:(  ___  )\\__   __/(       )                                                      :\r\n:| (   ) |   ) (   | () () |                                                      :\r\n:| (___) |   | |   | || || |                                                      :\r\n:|  ___  |   | |   | |(_)| |                                                      :\r\n:| (   ) |   | |   | |   | |                                                      :\r\n:| )   ( |   | |   | )   ( |                                                      :\r\n:|/     \\|   )_(   |/     \\|                                                      :\r\n···················································································\n");
-        static void ShowMainMenu(User loggedinUser)
+        public static void ShowMainMenu(User loggedinUser)
         {
             bool runMenu = true;
 
@@ -86,69 +84,9 @@ namespace OpenDefendersATM
                         break;
                 }
                 Console.WriteLine("Tryck Enter för att fortsätta...");
-                Console.ReadLine();
                 Console.Clear();
             }
         }
-
-        //meny för customer 
-        //public static void CustomerMenu(User user)
-        //{
-        //    bool running = true;
-        //    while (running)
-        //    {
-        //        Console.Clear();
-        //        Console.WriteLine($"\t[KUND] Inloggad som " + user.Name);
-        //        Console.WriteLine();
-        //        Console.WriteLine("Välj ett alternativ.");
-        //        Console.WriteLine(new string('*', 30));
-        //        Console.WriteLine("1. Visa konton.");
-        //        Console.WriteLine("2. Öppna nytt konto.");
-        //        Console.WriteLine("3. Överföring.");
-        //        Console.WriteLine("4. Lån.");
-        //        Console.WriteLine("5. Transaktionslog.");
-        //        Console.WriteLine("6. Logga ut.");
-        //        Console.WriteLine(new string('*', 30));
-
-        //        int input = Backup.ReadInt("Ditt val: ");
-        //        Customer? customer = user as Customer;
-        //        switch (input)
-        //        {
-        //            case 1:
-        //                customer?.ViewAccounts();
-        //                Console.WriteLine("Tryck Enter för att fortsätta...");
-        //                Console.ReadLine();
-        //                break;
-        //            case 2:
-        //                customer?.OpenAccount();
-        //                break;
-        //            case 3:
-        //                TransferInteraction(customer?.CustomerAccounts);
-        //                break;
-        //            case 4:
-        //                customer?.RequestLoan();
-        //                break;
-        //            case 5:
-        //                if (customer?.CustomerAccounts.Count > 0)
-        //                {
-
-        //                    customer?.CustomerAccounts[0].ViewAllTransactions();
-        //                }
-        //                else
-        //                {
-        //                    Console.WriteLine("Du har inga transaktioner.");
-        //                }
-        //                break;
-        //            case 6:
-        //                running = false;
-        //                break;
-
-        //            default:
-        //                Console.WriteLine("Felaktigt val.");
-        //                break;
-        //        }
-        //    }
-        //}
 
         public static void WithdrawInteraction(Account account)
         {
